e5b8b575d9299ecf79da22ffff66df32519c33d0
Mujtabo <s.mujtabo05@gmail.com>
2023-12-16 16:10:23 +0700

    Add new elements

--- dist/script.js
+++ dist/script.js
@@ -40,3 +40,22 @@ function updateClock() {
 }
 
 updateClock();
+
+// Carousel
+let currentIndex = 0;
+
+function showSlide(index) {
+  const carousel = document.getElementById("carousel");
+  const totalItems = document.querySelectorAll("#carousel-item").length;
+  currentIndex = (index + totalItems) % totalItems;
+  const translateValue = -currentIndex * 100 + "%";
+  carousel.style.transform = "translateX(" + translateValue + ")";
+}
+
+function nextSlide() {
+  showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+  showSlide(currentIndex - 1);
+}
