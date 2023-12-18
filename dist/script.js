66f0c5388285b14c936972d3a9a888658dd8d1b0
Mujtabo <s.mujtabo05@gmail.com>
2023-12-18 14:50:01 +0700

    first page is finished

--- dist/script.js
+++ dist/script.js
@@ -59,3 +59,52 @@ function nextSlide() {
 function prevSlide() {
   showSlide(currentIndex - 1);
 }
+
+// Login Button
+document.getElementById("loginButton").addEventListener("click", function () {
+  var loginForm = document.getElementById("loginForm");
+  var overlay = document.getElementById("overlay");
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+
+  if (loginForm.style.display === "block") {
+    loginForm.style.display = "none";
+    overlay.style.display = "none";
+  } else {
+    loginForm.style.display = "block";
+    overlay.style.display = "block";
+  }
+});
+
+function closeLoginForm() {
+  var loginForm = document.getElementById("loginForm");
+  var overlay = document.getElementById("overlay");
+  loginForm.style.display = "none";
+  overlay.style.display = "none";
+}
+
+// Close the login form when clicking outside the form
+document.addEventListener("click", function (event) {
+  var loginForm = document.getElementById("loginForm");
+  var overlay = document.getElementById("overlay");
+  var isClickInsideForm = loginForm.contains(event.target);
+  var isLoginButton = event.target.id === "loginButton";
+
+  if (!isClickInsideForm && !isLoginButton) {
+    loginForm.style.display = "none";
+    overlay.style.display = "none";
+  }
+});
+
+function login() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  // Perform login logic here
+  console.log("Email: " + email + ", Password: " + password);
+}
+
+function loginWithGoogle() {
+  // Perform Google login logic here
+  console.log("Logging in with Google");
+}
